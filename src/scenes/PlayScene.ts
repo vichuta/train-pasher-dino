0fd7b432b708fb002e3fa1c30548fb4014ccee75
MSI\Nice <vichuta.nice@gmail.com>
2024-08-19 15:44:27 +0700

    move function

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -11,13 +11,20 @@ class PlayScene extends Phaser.Scene {
   }
 
   create() {
+    this.createEnvironment() // add ground
+    this.createPlayer() // add Dino
+  }
+
+  createPlayer() {
+    // add Dino
+    this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1)
+  }
+
+  createEnvironment() {
     // add ground
     this.add
       .tileSprite(0, this.gameHeight as number, 88, 26, 'ground')
       .setOrigin(0, 1)
-
-    // add Dino
-    this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1)
   }
 }
 
