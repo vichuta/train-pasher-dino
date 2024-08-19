5fa80bd635b6280ee85545457ae1d873a497ac2a
MSI\Nice <vichuta.nice@gmail.com>
2024-08-19 15:37:47 +0700

    fix: get height game to use position y

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,13 +1,20 @@
 import Phaser from 'phaser'
 
 class PlayScene extends Phaser.Scene {
+  // get ค่าความสูงของเกม
+  get gameHeight() {
+    return this.game.config.height as number
+  }
+
   constructor() {
     super('PlayScene')
   }
 
   create() {
     this.game.config.height
-    this.add.tileSprite(0, 340, 1000, 26, 'ground').setOrigin(0, 1)
+    this.add
+      .tileSprite(0, this.gameHeight as number, 88, 26, 'ground')
+      .setOrigin(0, 1)
   }
 }
 
