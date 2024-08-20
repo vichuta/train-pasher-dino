e9f76495e9013762eaf16803566d67bf86315add
MSI\Nice <vichuta.nice@gmail.com>
2024-08-20 13:58:25 +0700

    fix: Preload prepare obstacle image

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import { PRELOAD_CONFIG } from '..'
 import { Player } from '../entities/Player'
 import { SpriteWithDynamicBody } from '../types'
 import { GameScene } from './GameScene'
@@ -96,7 +97,8 @@ class PlayScene extends GameScene {
   }
 
   spawnObstacle() {
-    const obstacleNum = Math.floor(Math.random() * 6) + 1
+    const obstacleNum =
+      Math.floor(Math.random() * PRELOAD_CONFIG.cactusesCount) + 1
     const distance = Phaser.Math.Between(600, 900)
 
     this.obstacles
