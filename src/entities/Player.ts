ad35f9788797f711e1debdb7d29900379915e8e3
MSI\Nice <vichuta.nice@gmail.com>
2024-08-20 10:12:25 +0700

    add: cursor input keyboard 'spacebar' to jump

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,4 +1,5 @@
 export class Player extends Phaser.Physics.Arcade.Sprite {
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys // CursorKeys เป็น type Object ที่เก็บ keyborad ขึ้น-ลง-ซ้าย-ขวา-spacebar
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'dino-idle')
 
@@ -7,10 +8,13 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     this.init()
     // this.registerPlayerControl()
+
+    // เมื่อเกิด event update ให้ทำ function "this.update" โดยใช้ update เฉพาะใน class นี้
     this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
   }
 
   init() {
+    this.cursors = this.scene.input.keyboard.createCursorKeys()
     this.setOrigin(0, 1)
       .setGravityY(5000)
       .setCollideWorldBounds(true)
@@ -18,7 +22,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   // registerPlayerControl() {
-  //   // ถ้ากด spacebar ให้ dino กระโดด
+  //   // ถ้ากด spacebar ให้ dino กระโดด *แต่กดค้าง = กระโดดครั้งเดียว
   //   const spaceBar = this.scene.input.keyboard.addKey(
   //     Phaser.Input.Keyboard.KeyCodes.SPACE
   //   )
@@ -28,6 +32,12 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   //   })
   // }
   update() {
-    console.log('Player update!')
+    const { space } = this.cursors
+    console.log(space.isDown)
+
+    //ถ้าปุ่ม spacebar ถูกกดอยู่ / กด spacebar ค้าง = โดดดดดดดดดดดดดดดดด ลอยเลย
+    if (space.isDown) {
+      this.setVelocityY(-1600)
+    }
   }
 }
