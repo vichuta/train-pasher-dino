294bf8f8fff9f164922124a05e83f38bf970033b
MSI\Nice <vichuta.nice@gmail.com>
2024-08-20 10:14:13 +0700

    add: function to check Player stand onFloor ?

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -35,9 +35,20 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     const { space } = this.cursors
     const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space)
 
+    const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor()
+    console.log(onFloor)
+
     //ถ้าปุ่ม spacebar ถูกกดอยู่ / กด spacebar ค้าง = โดดดดดดดดดดดดดดดดด ลอยเลย
     if (isSpaceJustDown) {
       this.setVelocityY(-1600)
     }
+
+    //  const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space)
+
+    //  const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor()
+
+    //  if (isSpaceJustDown && onFloor) {
+    //    this.setVelocityY(-1600)
+    //  }
   }
 }
